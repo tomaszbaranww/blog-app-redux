4d59470d75a0dca50e4599c7768c5dc630f6c7b1
PanWidmo <tomasz.baranww@gmail.com>
2023-05-23 13:36:17 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import 'styles/global.module.scss';
 import { PostsList } from 'features/posts/PostsList';
 import { AddPostForm } from 'features/posts/AddPostForm';
+import { Layout } from 'components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import { SinglePostPage } from 'features/posts/SinglePostPage';
 
 export const App = () => {
     return (
-        <main className="App">
-            <AddPostForm />
-            <PostsList />
-        </main>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<PostsList />} />
+                <Route path="post">
+                    <Route index element={<AddPostForm />} />
+                    <Route path=":postId" element={<SinglePostPage />} />
+                </Route>
+            </Route>
+        </Routes>
     );
 };
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { App } from 'App';
 import { store } from 'app/store';
 import { Provider } from 'react-redux';
 import { fetchUsers } from 'features/users/usersSlice';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 store.dispatch(fetchUsers());
 
@@ -11,7 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <Router>
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </Router>
         </Provider>
     </React.StrictMode>
 );
