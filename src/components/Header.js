2d706658e8aa1719c7c625944a09b8969784aeea
PanWidmo <tomasz.baranww@gmail.com>
2023-05-26 12:09:03 +0200

    Add counter to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { increaseCount, getCount } from 'features/posts/postsSlice';
+import styles from 'styles/Header.module.scss';
 
 export const Header = () => {
+    const dispatch = useDispatch();
+    const count = useSelector(getCount);
+
     return (
-        <header>
+        <header className={styles.header}>
             <h1>Blog app redux</h1>
-            <nav>
+            <nav className={styles.nav}>
                 <ul>
                     <li>
                         <Link to="/">Home</Link>
@@ -12,7 +18,11 @@ export const Header = () => {
                     <li>
                         <Link to="post">Post</Link>
                     </li>
+                    <li>
+                        <Link to="user">Users</Link>
+                    </li>
                 </ul>
+                <button onClick={() => dispatch(increaseCount())}>{count}</button>
             </nav>
         </header>
     );
