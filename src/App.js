405649fd519762b0ff9320643aa7162589e6ab89
PanWidmo <tomasz.baranww@gmail.com>
2023-05-26 12:08:25 +0200

    Create UsersList

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import 'styles/global.module.scss';
 import { PostsList } from 'features/posts/PostsList';
 import { AddPostForm } from 'features/posts/AddPostForm';
 import { Layout } from 'components/Layout';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { SinglePostPage } from 'features/posts/SinglePostPage';
 import { EditPostForm } from 'features/posts/EditPostForm';
+import { UsersList } from 'features/users/UsersList';
+import { UserPage } from 'features/users/UserPage';
 
 export const App = () => {
     return (
@@ -16,6 +18,13 @@ export const App = () => {
                     <Route path=":postId" element={<SinglePostPage />} />
                     <Route path="edit/:postId" element={<EditPostForm />} />
                 </Route>
+                <Route path="user">
+                    <Route index element={<UsersList />} />
+                    <Route path=":userId" element={<UserPage />} />
+                </Route>
+
+                {/*Catch all*/}
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Route>
         </Routes>
     );
