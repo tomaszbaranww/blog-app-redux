4002d58f39ee328d142850d4ebf6515e3eec980f
PanWidmo <tomasz.baranww@gmail.com>
2023-07-31 14:01:13 +0200

    Remove unnecessary quote marks

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -9,7 +9,7 @@ export const PostsList = () => {
     let content;
 
     if (postStatus === 'loading') {
-        content = <p>"Loading..."</p>;
+        content = <p>Loading...</p>;
     } else if (postStatus === 'succeeded') {
         content = orderedPostIds.map((postId) => <PostsExcerpt key={postId} postId={postId} />);
     } else if (postStatus === 'failed') {
